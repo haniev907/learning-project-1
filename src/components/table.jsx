98af3fa58c6256a434483bd4a0dd87a8076df9ef
haniev907 <35073345+haniev907@users.noreply.github.com>
2022-07-18 16:21:58 +0300

    Форма добавления пользователя

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,56 +1,72 @@
-import React, {useState} from 'react';
-import api from '../api'
+import React, { useState } from "react";
+import api from "../api";
 
+import AddForm from './AddForm';
 
 const MyTableInfo = () => {
+  const [users, setUsers] = useState(api.users.fetchAll());
+  const [isAdding, setIsAdding] = useState(false);
 
-    const[users, setUsers] = useState(api.users.fetchAll())
-    const handleDelete = (userId) => {
-        setUsers((users.filter((user) => user._id !== userId)))
-    };
+  const handleDelete = (userId) => {
+    setUsers(users.filter((user) => user._id !== userId));
+  };
 
-    function resultTableAfterDeletingAllUsers(){
-        return <div className='badge bg-primary text-wrap Text_wrap'>
-        Таблица пуста
-    </div>
-    };
-
+  function resultTableAfterDeletingAllUsers() {
     return (
-        <>
-            {users.length === 0 ? resultTableAfterDeletingAllUsers():
+      <div className="badge bg-primary text-wrap Text_wrap">Таблица пуста</div>
+    );
+  }
+
+  const addUser = (userData) => {
+    setUsers((users) => users.concat(userData));
+  };
+
+  return (
+    <>
+      {users.length === 0 ? (
+        resultTableAfterDeletingAllUsers()
+      ) : (
         <table class="table table table-dark table-striped">
-  <thead>
-    <tr>
-    <th scope="col">Имя</th>
-        <th scope="col">Качество</th>
-        <th scope="col">Профессия</th>
-        <th scope="col">Оценка</th>
-        <th></th>
-    </tr>
-  </thead>
-  <tbody>
-        {users.map((user) => (
-        <tr key={user._id}>
-            <td className=''>{user.name}</td>
-            <td className='td_table'>{user.qualities}</td>
-            <td className=''>{user.profession}</td>
-            <td className=''>{user.rate}</td>
-            <td className='table-light'><button className = {'btn btn-danger delete_Button'} onClick = {() => handleDelete(user._id)}>
-                Удалить
-                </button></td>
-        </tr>
-        ))}
-    </tbody>
-    </table>
-    
-    }
-
-    <button className = {'btn btn-primary Add_Button'}>Добавить пользователя</button>
-
-        </>
-        
-    )
-
-}
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качество</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Оценка</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td className="">{user.name}</td>
+                <td className="td_table">{user.qualities}</td>
+                <td className="">{user.profession}</td>
+                <td className="">{user.rate}</td>
+                <td className="table-light">
+                  <button
+                    className={"btn btn-danger delete_Button"}
+                    onClick={() => handleDelete(user._id)}
+                  >
+                    Удалить
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      <button
+        className={"btn btn-primary Add_Button"}
+        onClick={() => {
+          setIsAdding(true);
+        }}
+      >
+        Добавить пользователя
+      </button>
+      {isAdding ? <AddForm addUser={addUser} /> : null}
+    </>
+  );
+};
 
 export default MyTableInfo;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import reactDom from 'react-dom';
 import './index.css'
-import 'bootstrap'
 import MyTableInfo from "./components/table";
 const App = () => {
     return <MyTableInfo/>;
